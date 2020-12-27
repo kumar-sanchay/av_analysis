c62aee3601b7b3e3f60f74fe22ccfbc6d5d98d1c
Sanchay Kumar <dsanchaykumar@gmail.com>
2020-12-27 19:32:43 +0530

    url changes

--- index.js
+++ index.js
@@ -19,10 +19,9 @@ app.use(BodyParser.urlencoded({ extended: true }))
 const postData = require('./controllers/postData')
 const getData = require('./controllers/getData')
 const teamMetric = require('./controllers/metric/teamMetric')
-const dummy = require('./controllers/dummy')
 
-app.post('/api/post-room-data', postData)
-app.get('/api/get-room-data', getData)
-app.get('/api/teamMetric', teamMetric)
-app.get('/api/dummy', dummy)
+app.post('/api/analysis/post-room-data', postData)
+app.get('/api/analysis/get-room-data', getData)
+app.get('/api/analysis/teamMetric', teamMetric)
+
 
